88f97300fee8eeb2032e784bb6cc4603a8e886c8
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2021-07-18 21:34:52 +0100

    polishing

--- atada-scripts/atada/init-testnet.sh
+++ atada-scripts/atada/init-testnet.sh
@@ -4,7 +4,3 @@ set -x
 
 # Copying the atada script common config to user's home
 cp testnet/.common.inc ~/
-
-# Add scripts to path for ease of use
-echo "Execute the line below so to have scripts in your path"
-echo "export PATH=/home/cardano/scripts/cardano/testnet:$PATH"

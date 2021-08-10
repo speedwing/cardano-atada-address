1936e2bb48a17d67f5a6b19c888fe3a2d273bcc5
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2021-08-10 22:25:03 +0100

    new generate kyes

--- generate-keys.sh
+++ generate-keys.sh
@@ -7,26 +7,96 @@ if [ ! -f ./phrase.prv ]; then
   exit 1
 fi
 
-cardano-address key from-recovery-phrase Shelley < phrase.prv > root.xsk
+function cardano-address-testnet-init() {
 
-cardano-address key child 1852H/1815H/0H/0/0 < root.xsk | cardano-address key public --with-chain-code > addr.xvk
+  NETWORK_TAG=testnet
+  MAGIC="--testnet-magic 1097911063"
 
-cardano-address key child 1852H/1815H/0H/2/0 < root.xsk | cardano-address key public --with-chain-code > stake.xvk
+}
 
-cardano-address address payment --network-tag mainnet < addr.xvk > many.payment.addr
+function cardano-address-mainnet-init() {
 
-cardano-address address delegation $(cat stake.xvk) < many.payment.addr > payment-delegated.addr
+  NETWORK_TAG=mainnet
+  MAGIC="--mainnet"
 
-cardano-address address stake --network-tag mainnet < stake.xvk > many.stake.addr
+}
 
-cardano-cli key convert-cardano-address-key --shelley-payment-key --signing-key-file root.xsk --out-file many.payment.skey
-cardano-cli key verification-key --signing-key-file many.payment.skey --verification-key-file many.payment.vkey
+function cardano-address-create-wallet() {
 
-## Maybe not required
-# cardano-cli key non-extended-key --extended-verification-key-file payment.vkey --verification-key-file payment-non-extended.vkey
+  cardano-address-mainnet-init
 
-cardano-cli key convert-cardano-address-key --shelley-stake-key --signing-key-file root.xsk --out-file many.stake.skey
-cardano-cli key verification-key --signing-key-file many.stake.skey --verification-key-file many.stake.vkey
+  WALLET_DIR=${HOME}
+  ROOT_PRIV_KEY_FILE=${WALLET_DIR}/root.prv
+  MNEMONICS_PRIV_FILE=${WALLET_DIR}/phrase.prv
+  STAKE_XPRIV_KEY_FILE=${WALLET_DIR}/stake.xprv
+  STAKE_XPUB_KEY_FILE=${WALLET_DIR}/stake.xpub
+  STAKE_ADDRESS_FILE=${WALLET_DIR}/many.staking.addr
+  STAKE_CERT_FILE=${WALLET_DIR}/many.staking.cert
+  STAKE_SKEY_FILE=${WALLET_DIR}/many.staking.skey
+  STAKE_EVKEY_FILE=${WALLET_DIR}/many.staking.evkey
+  STAKE_VKEY_FILE=${WALLET_DIR}/many.staking.vkey
+  PAYMENT_XPRV_KEY_FILE=${WALLET_DIR}/payment.xprv
+  PAYMENT_XPUB_KEY_FILE=${WALLET_DIR}/payment.xpub
+  PAYMENT_ADDRESS_FILE=${WALLET_DIR}/payment.addr
+  PAYMENT_SKEY_FILE=${WALLET_DIR}/many.payment.skey
+  PAYMENT_EVKEY_FILE=${WALLET_DIR}/many.payment.evkey
+  PAYMENT_VKEY_FILE=${WALLET_DIR}/many.payment.vkey
+  DELEGATION_ADDRESS_FILE=${WALLET_DIR}/delegation.addr
+  BASE_ADDRESS_CANDIDATE_FILE=${WALLET_DIR}/base.addr_candidate
+  BASE_ADDRESS_FILE=${WALLET_DIR}/base.addr
 
-## Maybe not required
-# cardano-cli key non-extended-key --extended-verification-key-file stake.vkey --verification-key-file stake-non-extended.vkey
+  # Generate mnemonics and root key
+  cat ${MNEMONICS_PRIV_FILE} | \
+    cardano-address key from-recovery-phrase Shelley > ${ROOT_PRIV_KEY_FILE}
+  # Generate account keys
+  cat ${ROOT_PRIV_KEY_FILE} | cardano-address key child 1852H/1815H/0H/0/0 > ${PAYMENT_XPRV_KEY_FILE}
+  cat ${PAYMENT_XPRV_KEY_FILE} | cardano-address key public --with-chain-code | tee ${PAYMENT_XPUB_KEY_FILE} | \
+    cardano-address address payment --network-tag ${NETWORK_TAG} > ${PAYMENT_ADDRESS_FILE}
+  cat ${PAYMENT_XPRV_KEY_FILE} | cardano-address key inspect > ${PAYMENT_SKEY_FILE}
+  # Generate stake keys
+  cat ${ROOT_PRIV_KEY_FILE} | cardano-address key child 1852H/1815H/0H/2/0 > ${STAKE_XPRIV_KEY_FILE}
+  cat ${STAKE_XPRIV_KEY_FILE} | \
+    cardano-address key public --with-chain-code | tee ${STAKE_XPUB_KEY_FILE} | \
+    cardano-address address stake --network-tag ${NETWORK_TAG} > ${STAKE_ADDRESS_FILE}
+  cat ${STAKE_XPRIV_KEY_FILE} | cardano-address key inspect > ${STAKE_SKEY_FILE}
+  cat ${PAYMENT_XPRV_KEY_FILE} | \
+    cardano-address key public --with-chain-code | \
+    cardano-address address payment --network-tag ${NETWORK_TAG} | \
+    cardano-address address delegation $(cat ${STAKE_XPRIV_KEY_FILE} | cardano-address key public --with-chain-code | tee ${STAKE_XPUB_KEY_FILE}) > ${BASE_ADDRESS_CANDIDATE_FILE}
+  # Inspired by https://gist.github.com/ilap/5af151351dcf30a2954685b6edc0039b#script
+  SESKEY=$( cat ${STAKE_XPRIV_KEY_FILE} | bech32 | cut -b -128 )$( cat ${STAKE_XPUB_KEY_FILE} | bech32)
+  PESKEY=$( cat ${PAYMENT_XPRV_KEY_FILE} | bech32 | cut -b -128 )$( cat ${PAYMENT_XPUB_KEY_FILE} | bech32)
+
+  cat << EOF2 > ${STAKE_SKEY_FILE}
+{
+    "type": "StakeExtendedSigningKeyShelley_ed25519_bip32",
+    "description": "",
+    "cborHex": "5880$SESKEY"
+}
+EOF2
+
+  cat << EOF3 > ${PAYMENT_SKEY_FILE}
+{
+    "type": "PaymentExtendedSigningKeyShelley_ed25519_bip32",
+    "description": "Payment Signing Key",
+    "cborHex": "5880$PESKEY"
+}
+EOF3
+
+  cardano-cli key verification-key --signing-key-file ${STAKE_SKEY_FILE} --verification-key-file ${STAKE_EVKEY_FILE}
+  cardano-cli key verification-key --signing-key-file ${PAYMENT_SKEY_FILE} --verification-key-file ${PAYMENT_EVKEY_FILE}
+
+  cardano-cli key non-extended-key --extended-verification-key-file ${STAKE_EVKEY_FILE} --verification-key-file ${STAKE_VKEY_FILE}
+  cardano-cli key non-extended-key --extended-verification-key-file ${PAYMENT_EVKEY_FILE} --verification-key-file ${PAYMENT_VKEY_FILE}
+
+  cardano-cli stake-address build --stake-verification-key-file ${STAKE_VKEY_FILE} $MAGIC > ${STAKE_ADDRESS_FILE}
+  cardano-cli stake-address registration-certificate --stake-verification-key-file ${STAKE_VKEY_FILE} --out-file ${STAKE_CERT_FILE}
+  cardano-cli address build --payment-verification-key-file ${PAYMENT_VKEY_FILE} $MAGIC > ${PAYMENT_ADDRESS_FILE}
+  cardano-cli address build \
+      --payment-verification-key-file ${PAYMENT_VKEY_FILE} \
+      --stake-verification-key-file ${STAKE_VKEY_FILE} \
+      $MAGIC > ${BASE_ADDRESS_FILE}
+
+}
+
+cardano-address-create-wallet

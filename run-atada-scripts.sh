23c858e7942a78901ffa0bb790fba83033d30c3f
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2021-11-12 21:29:35 +0000

    stuff

--- run-atada-scripts.sh
+++ run-atada-scripts.sh
@@ -6,7 +6,21 @@ OS_ARCH=$(uname -m)
 NODE_VERSION="1.30.1"
 IMAGE_TAG="${NODE_VERSION}-${OS_ARCH}"
 
-docker run -it --rm \
-    -v /Users/MEKES01/tmp/db:/db \
+NETWORK=mainnet
+CARDANO_NODE_PORT=3000
+
+DB_FOLDER=/Users/MEKES01/tmp/db
+#DB_FOLDER=/Users/giovanni/.cardano/cardano-mainnet
+
+docker run -d --rm \
+    --name cardano-node-mainnet \
+    -v ${DB_FOLDER}:/db \
     -v `pwd`/phrase.prv:/root/phrase.prv \
-    "atada-scripts:${NODE_VERSION}-${OS_ARCH}" bash
+    "atada-scripts:${IMAGE_TAG}" \
+    "cardano-node run \
+    --topology /etc/config/${NETWORK}-topology.json \
+    --database-path /db \
+    --socket-path /db/node.socket \
+    --host-addr 0.0.0.0 \
+    --port $CARDANO_NODE_PORT \
+    --config /etc/config/${NETWORK}-config.json"
